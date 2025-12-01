fd32ddea84338a7ca9ca1fde5dd73696af8ca246
LeandroDeJesus-S <jstleandro@gmail.com>
2025-12-01 20:11:59 -0300

    add custom prompt and spinner for code companion

--- lua/plugins/code-companion.lua
+++ lua/plugins/code-companion.lua
@@ -4,6 +4,7 @@ return {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
         "nvim-telescope/telescope.nvim",
+        "franco-ruggeri/codecompanion-spinner.nvim",
     },
     keys = {
         {
@@ -24,12 +25,15 @@ return {
                     provider = "telescope",
                 },
             },
+            extensions = {
+                spinner = {},
+            },
             strategies = {
                 chat = {
                     adapter = "gemini",
                     slash_commands = {
                         ["file"] = {
-                            callback = "strategies.chat.slash_commands.file",
+                            callback = "strategies.chat.slash_commands.catalog.file",
                             description = "Select a file using Telescope",
                             opts = {
                                 provider = "telescope", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks"
@@ -71,6 +75,48 @@ return {
                             }
                         )
                     end,
+                },
+            },
+            prompt_library = {
+                ["Convertional Commit"] = {
+                    strategy = "chat",
+                    description = "Suggests a semantic/convertional commit message",
+                    prompts = {
+                        role = "user",
+                        content = function(context)
+                            local tpl =
+                                [[Generate a high-quality commit message following the rules of Conventional Commits (https://www.conventionalcommits.org/).
+
+Requirements:
+- Start with a valid type (feat, fix, refactor, docs, style, test, chore, perf, build, ci, revert).
+- Optionally include a scope in parentheses.
+- Use a short, clear, one-line description written in the imperative mood (e.g., “add…”, “fix…”, “update…”).
+- Keep the subject under 70 characters when possible.
+
+Body rules:
+- Provide a concise and meaningful explanation of WHAT changed and WHY.
+- Break the body into paragraphs if needed, each focused and short.
+- Do not repeat the subject in the body.
+- Avoid implementation details unless they help understand intent.
+- If relevant, include notes on breaking changes or important side effects.
+
+Input:
+The following are the staged changes; analyze them carefully and infer the most appropriate type, scope, and description:
+
+```diff
+%s
+```
+]]
+                            local diff = vim.system(
+                                { "git", "diff", "--no-ext-diff", "--staged" },
+                                { text = true }
+                            ):wait()
+                            return string.format(tpl, diff.stdout)
+                        end,
+                        opts = {
+                            contains_code = true,
+                        },
+                    },
                 },
             },
         })
